93c6c5e5fd48ab1419b75a1ed2ac8202d8588bfb
aARTEMkad <artemkovel2007@gmail.com>
2023-10-29 13:29:08 +0100

    -

--- src/controller/ShoesController.js
+++ src/controller/ShoesController.js
@@ -107,7 +107,38 @@ exports.DeleteShoes = async (req, res) => {
     }
 }
 
-exports.UpdateShoes = async (req, res) => {
-    
+
+
+exports.UpdateShoes = async (req, res) => { // JSON
+    const newShoes = new ShoesModel({
+        name: req.body.name,                
+        product: req.body.product,
+        price: req.body.price,
+        size: req.body.size,
+        vendorcode: req.body.vendorcode,
+        color: req.body.color,
+
+        front_photo: {
+            filename: req.files[0].filename,
+            originalname: req.files[0].originalname,
+            path: req.files[0].path
+        },
+        back_photo: {
+            filename: req.files[1].filename,
+            originalname: req.files[1].originalname,
+            path: req.files[1].path
+        },
+        top_photo: {
+            filename: req.files[2].filename,
+            originalname: req.files[2].originalname,
+            path: req.files[2].path
+        },
+        aspect_photo: {
+            filename: req.files[3].filename,
+            originalname: req.files[3].originalname,
+            path: req.files[3].path
+        }
+    })
+    ShoesModel.findByIdAndUpdate(req.params.id, newShoes)
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -33,6 +33,7 @@ Router.post('/api/shoes/add', upload.array('PhotoShoes', 4), AddShoes)
 
 Router.delete('/api/shoes/delete/:id', DeleteShoes)
 
+Router.put('/api/shoes/update/:id', upload.array('PhotoShoes', 4), UpdateShoes)
 
 
 
