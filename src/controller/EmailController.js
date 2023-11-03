2fbf85ec3adbedbb8235d0054c758404782993ee
aARTEMkad <artemkovel2007@gmail.com>
2023-11-03 22:57:02 +0100

    create EmailController function 'SendEmailMessage' and 'GetAllEmailMessage'

--- src/controller/EmailController.js
+++ src/controller/EmailController.js
@@ -1,5 +1,37 @@
 const EmailMessageModel = require('../model/_emailMessage')
 
 
+exports.SendEmailMessage = async (req, res) => {
+    try{
+        const emailMessage = new EmailMessageModel({
+            fullName: req.body.fullName,
+            numberPhone: req.body.numberPhone,
+            email: req.body.email,
+            message: req.body.message
+        })
 
+        emailMessage.save().then(() => {
+            res.status(201)
+            res.setHeader('Content-Type', 'application/json')
+            res.json(emailMessage)
+        }).catch((err) => {
+            console.log(err)
+            res.status(502)
+        })
+    } catch(err) {
+        console.log(err)
+        res.status(404)
+    }
+}
 
+exports.GetAllEmailMessage = async (req, res) => {
+    try{
+        const allEmailMessage = await EmailMessageModel.find({})
+
+        res.status(200)
+        res.setHeader('Content-Type', 'application/json')
+        res.json(allEmailMessage)
+    } catch(err) {
+        console.log(err)
+    }
+}
