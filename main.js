d3cf9313c1c441dd71b3da13f97d62ff70023bc3
aARTEMkad <artemkovel2007@gmail.com>
2023-11-13 19:43:22 +0100

    Add UserRouter and 2 request

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = express();
 const Router = require('./src/routes/routes')
 const emailRouter = require('./src/routes/emailRoutes')
 const RegistraScription = require('./src/routes/RegistraScriptionRouter')
+const userRouter = require('./src/routes/UserRouter')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 3001
@@ -16,6 +17,7 @@ mongoose.connect(process.env.CONNECT_DB).then(() => {
     app.use(Router)
     app.use(emailRouter)
     app.use(RegistraScription)
+    app.use(userRouter)
     app.use(express.urlencoded({extended: true}))
 
     app.listen(PORT, (req) => {
